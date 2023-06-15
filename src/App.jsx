33faa662fa9497a48d286f5b2f4938b6dea1071d
roman <romansheikh170@gmail.com>
2023-06-16 02:21:00 +0600

    dlt

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import Router from "./Routers/Routers";
 import "./App.css";
 
 function App() {
+  const app = {
+    background: "url('assets/image/bg_main.gif')",
+    backgroundPosition: "center"
+
+  }
   return (
     <>
-      <div className="app">
+      <div id="app" style={app}>
         <div className="content">
           <RouterProvider router={Router}> </RouterProvider>
         </div>
